09fa80cf771928eceb75228c53ded83823c3081f
pablo-pazzaglia <pazzagliapablo94@gmail.com>
2024-09-26 22:10:17 +0200

    + Modifica su store

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,30 @@ import './App.css';
 import Movie from "./pages/Movie";
 import SinglePage from "./pages/singlePage/SinglePage";
 import Mode from "./components/mode/Mode";
-import ThemeContext from './store/themeContext';
-import ThemeProvider from './store/themeContext';
-import { useContext } from 'react';
+import { useContext, createContext, useState, useEffect } from 'react';
+
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem('theme') || 'light';
+  });
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
 function App() {
   return (
