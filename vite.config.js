9150fb22f93d2736f2dfb80ddc07a5c1ac3a0946
pablo-pazzaglia <pazzagliapablo94@gmail.com>
2024-09-27 01:27:47 +0200

    + modifica su config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
+const viteEnv = {}
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
+  }
 })
+export default {
+  alias: {
+    '@': require('path').resolve(__dirname, 'src')
+  },
+  define: viteEnv,
+  plugins: [
+    react()
+  ]
+}
 
